0910b5cf3cefa5014208ab2930b605a11aca1ec1
Zulaikha Zakiullah <zzakiull@uwaterloo.ca>
2021-04-13 18:25:30 -0400

    Make links modular

--- js/script.js
+++ js/script.js
@@ -1,5 +1,19 @@
+var links = {
+    "github": "https://github.com/MalickReza",
+    "email": "mailto:mmrezajuly@gmail.com",
+    "linkedin": "https://www.linkedin.com/in/malickmusfiquereza"
+};
+
+function setNavLink(element, key) {
+    element.href = links[key];
+}
+
 function initViz(project) {
     var placeholderDiv = document.getElementById("tableauViz");
     var url = "https://public.tableau.com/views/" + project;
     viz = new tableau.Viz(placeholderDiv, url);
 }
+
+$(".nav-icon").each(function() {
+    $(this).attr("href", links[$(this).attr("aria-label")]);
+});
